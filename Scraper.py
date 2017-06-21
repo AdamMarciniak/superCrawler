2753319c5b1a7449af4e68f0c8be60d9453d9446
Adam Marciniak <adam668@gmail.com>
2017-06-21 15:45:56 -0700

    Added files

---
+++ Scraper.py
@@ -0,0 +1,51 @@
+from bs4 import BeautifulSoup
+import requests
+import re
+
+nutriRegex = re.compile(r'((\w*\s\w*)|(\w*))\s(\d*\.\d*)\s(g|mg)')
+
+from pprint import pprint
+
+res = requests.get(r'https://www.realcanadiansuperstore.ca/Food/Fruits-%26-Vegetables/Fruit/Apples-%26-Pears/Anjou-Pears/p/20174514001_KG?isPDPFlow=Y')
+
+soup = BeautifulSoup(res.text, "lxml")
+#print(soup)
+
+elems = soup.select("span.nutrition-label")
+
+parsedList = soup.find_all("div", class_="main-nutrition-attr first")
+
+nutriDict = {}
+nutriList =[]
+
+for nutriTag in soup.find_all("div", class_="main-nutrition-attr first"):
+
+    rawData = (" ".join(nutriTag.text.split())[:-3])
+
+    regResult = nutriRegex.search(rawData)
+
+    label = regResult.group(1)
+
+    #Converts to mg
+    if 'mg' not in regResult.group(5):
+        label = (regResult.group(1))
+        nutriValue = (float(regResult.group(4)) * 1000.0)
+
+    else:
+        nutriValue = float(regResult.group(4))
+
+    nutriDict[label] = nutriValue
+
+
+
+
+print(nutriDict)
+
+
+
+
+
+
+
+
+
---
+++ foodParser.py
@@ -0,0 +1,69 @@
+from bs4 import BeautifulSoup
+import requests
+import re
+
+nutriRegex = re.compile(r'((\w*\s\w*)|(\w*))\s(\d*\.\d*)\s(g|mg)')
+
+from pprint import pprint
+
+res = requests.get(r'https://www.realcanadiansuperstore.ca/food')
+
+soup = BeautifulSoup(res.text, "lxml")
+#print(soup)
+
+
+nutriDict = {}
+nutriList =[]
+i = 0
+mainLinkList = []
+
+
+for nutriTag in soup.find_all("a", class_="sub-nav-link"):
+
+    link = nutriTag.get('href')
+
+    if link in mainLinkList:
+        continue
+
+    mainLinkList.append(link)
+
+
+    if '/Food/' in link:
+
+        res2 = requests.get('https://www.realcanadiansuperstore.ca'+ link)
+        soup2 = BeautifulSoup(res2.text, "lxml")
+        i+=1
+
+        if i == 100:
+            break
+        print(i)
+
+
+        for foodItem in soup2.find_all("a", class_="product-name"):
+
+            nutriList.append((foodItem.get('href')))
+            #print(len(nutriList))
+
+
+            # foodLink = foodItem.get('href')
+            # print(foodLink)
+
+notepad = open("URLLinks.txt", 'w')
+
+for line in nutriList:
+    notepad.write("%s\n" % line)
+
+print(len(list(nutriList)))
+
+
+
+
+
+
+
+
+
+
+
+
+
